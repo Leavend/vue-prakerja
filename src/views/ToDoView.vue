c6533108573ec6eb09a4d892dda61ef8ee3cb271
Leavend <tiohadybayu@gmail.com>
2023-07-28 22:09:42 +0800

    little

---
+++ src/views/ToDoView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="todo">
+        <TodoList title="Todo List" />
+    </div>
+</template>
+
+<script>
+import TodoList from '@/components/ToDoList.vue';
+export default {
+    name: 'TodoView',
+    components: {
+        TodoList
+    }
+}
+</script>
